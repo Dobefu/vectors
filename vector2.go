57cd9e1ea8928e14debfab133c26af83fb9df2e2
Connor van Spronssen <info@dobefu.com>
2025-07-20 16:55:10 +0200

    chore: Add doc comments to the vectors

--- vector2.go
+++ vector2.go
@@ -4,6 +4,7 @@ import (
 	"math"
 )
 
+// IVector2 is the interface for a 2D vector.
 type IVector2 interface {
 	Add(vec Vector2)
 	Sub(vec Vector2)
@@ -20,6 +21,7 @@ type IVector2 interface {
 	ToVector3() Vector2
 }
 
+// Vector2 provides a Vector2.
 type Vector2 struct {
 	IVector2
 
@@ -27,31 +29,37 @@ type Vector2 struct {
 	Y float64
 }
 
+// Add adds the values of another vector to this one.
 func (v *Vector2) Add(vec Vector2) {
 	v.X += vec.X
 	v.Y += vec.Y
 }
 
+// Sub subtracts the values of another vector to this one.
 func (v *Vector2) Sub(vec Vector2) {
 	v.X -= vec.X
 	v.Y -= vec.Y
 }
 
+// Mul multiplies the values of another vector to this one.
 func (v *Vector2) Mul(vec Vector2) {
 	v.X *= vec.X
 	v.Y *= vec.Y
 }
 
+// Div divides the values of this vector by those of the other vector.
 func (v *Vector2) Div(vec Vector2) {
 	v.X /= vec.X
 	v.Y /= vec.Y
 }
 
+// Bounce inverts the direction of the vector.
 func (v *Vector2) Bounce() {
 	v.X = -v.X
 	v.Y = -v.Y
 }
 
+// Normalize scales the vector magnitude to 1.
 func (v *Vector2) Normalize() {
 	magnitude := math.Sqrt(v.X*v.X + v.Y*v.Y)
 
@@ -61,24 +69,29 @@ func (v *Vector2) Normalize() {
 	}
 }
 
+// AngleRadians returns the angle of the vector in radians.
 func (v Vector2) AngleRadians() float64 {
 	return math.Atan2(v.Y, v.X)
 }
 
+// AngleDegrees returns the angle of the vector in degrees.
 func (v Vector2) AngleDegrees() float64 {
 	angle := math.Atan2(v.Y, v.X) * 180 / math.Pi
 
 	return math.Mod(angle+360, 360)
 }
 
+// IsZero returns whether or not the vector is zero on all axes.
 func (v *Vector2) IsZero() bool {
 	return v.X == 0 && v.Y == 0
 }
 
+// Magnitude returns the length of the vector.
 func (v *Vector2) Magnitude() float64 {
 	return math.Sqrt((v.X * v.X) + (v.Y * v.Y))
 }
 
+// ClampMagnitude limits the magnitude of the vector to a maximum value.
 func (v *Vector2) ClampMagnitude(maxValue float64) {
 	magnitude := v.Magnitude()
 
@@ -94,11 +107,13 @@ func (v *Vector2) ClampMagnitude(maxValue float64) {
 	})
 }
 
+// Clear sets the vector to zero on all axes.
 func (v *Vector2) Clear() {
 	v.X = 0
 	v.Y = 0
 }
 
+// ToVector3 converts the vector to a Vector3 with a Z value of 0.
 func (v Vector2) ToVector3() Vector3 {
 	return Vector3{
 		X: v.X,
--- vector3.go
+++ vector3.go
@@ -4,6 +4,7 @@ import (
 	"math"
 )
 
+// IVector3 is the interface for a 3D vector.
 type IVector3 interface {
 	Add(vec Vector3)
 	Sub(vec Vector3)
@@ -20,6 +21,7 @@ type IVector3 interface {
 	ToVector2() Vector2
 }
 
+// Vector3 provides a Vector3.
 type Vector3 struct {
 	IVector3
 
@@ -28,36 +30,42 @@ type Vector3 struct {
 	Z float64
 }
 
+// Add adds the values of another vector to this one.
 func (v *Vector3) Add(vec Vector3) {
 	v.X += vec.X
 	v.Y += vec.Y
 	v.Z += vec.Z
 }
 
+// Sub subtracts the values of another vector to this one.
 func (v *Vector3) Sub(vec Vector3) {
 	v.X -= vec.X
 	v.Y -= vec.Y
 	v.Z -= vec.Z
 }
 
+// Mul multiplies the values of another vector to this one.
 func (v *Vector3) Mul(vec Vector3) {
 	v.X *= vec.X
 	v.Y *= vec.Y
 	v.Z *= vec.Z
 }
 
+// Div divides the values of this vector by those of the other vector.
 func (v *Vector3) Div(vec Vector3) {
 	v.X /= vec.X
 	v.Y /= vec.Y
 	v.Z /= vec.Z
 }
 
+// Bounce inverts the direction of the vector.
 func (v *Vector3) Bounce() {
 	v.X = -v.X
 	v.Y = -v.Y
 	v.Z = -v.Z
 }
 
+// Normalize scales the vector magnitude to 1.
 func (v *Vector3) Normalize() {
 	magnitude := math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
 
@@ -68,24 +76,29 @@ func (v *Vector3) Normalize() {
 	}
 }
 
+// AngleRadians returns the angle of the vector in radians.
 func (v Vector3) AngleRadians() float64 {
 	return math.Atan2(v.Y, v.X)
 }
 
+// AngleDegrees returns the angle of the vector in degrees.
 func (v Vector3) AngleDegrees() float64 {
 	angle := math.Atan2(v.Y, v.X) * 180 / math.Pi
 
 	return math.Mod(angle+360, 360)
 }
 
+// IsZero returns whether or not the vector is zero on all axes.
 func (v *Vector3) IsZero() bool {
 	return v.X == 0 && v.Y == 0 && v.Z == 0
 }
 
+// Magnitude returns the length of the vector.
 func (v *Vector3) Magnitude() float64 {
 	return math.Sqrt((v.X * v.X) + (v.Y * v.Y) + (v.Z * v.Z))
 }
 
+// ClampMagnitude limits the magnitude of the vector to a maximum value.
 func (v *Vector3) ClampMagnitude(maxValue float64) {
 	magnitude := v.Magnitude()
 
@@ -102,12 +115,14 @@ func (v *Vector3) ClampMagnitude(maxValue float64) {
 	})
 }
 
+// Clear sets the vector to zero on all axes.
 func (v *Vector3) Clear() {
 	v.X = 0
 	v.Y = 0
 	v.Z = 0
 }
 
+// ToVector2 converts the vector to a Vector2, discarding the Z value.
 func (v Vector3) ToVector2() Vector2 {
 	return Vector2{
 		X: v.X,
